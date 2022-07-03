9144f180588402607769f52c0b038173d542ceaf
saladlemon <107619587+saladlemon@users.noreply.github.com>
2022-07-03 11:31:12 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+if -1000 <= x <= 1000 and -1000 <= y <= 1000:
+    if x>0 and y > 0:
+        print('1')
+    elif x < 0 and y > 0:
+        print('2')
+    elif x < 0 and y < 0:
+        print('3')
+    elif x > 0 and y < 0:
+        print('4')
+        
