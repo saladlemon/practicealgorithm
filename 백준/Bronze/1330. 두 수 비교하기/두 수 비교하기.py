aa59bf009bc4e6999515c2b4d0d57a8aa967193c
saladlemon <107619587+saladlemon@users.noreply.github.com>
2022-07-03 10:22:21 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,8 @@
+A, B = map(int, input().split())
+if -10000 <= A <= 10000 and -10000 <= B <= 10000:
+    if A > B:
+        print('>')
+    elif A < B:
+        print('<')
+    elif A == B:
+        print('==')
