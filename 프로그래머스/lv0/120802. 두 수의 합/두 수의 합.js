3757d6f3340ebd0c95124652fddf09c7d79a8c7d
DoHun Jeon <107619587+saladlemon@users.noreply.github.com>
2023-04-14 18:20:06 +0900

    [level 0] Title: 두 수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+    var wrong = null;
+    if (-50000 <= num1 && num1<= 50000){
+        if (-50000 <= num2 && num2<= 50000 ){
+            var answer = num1 + num2;
+        }
+    } 
+    return answer;
+}
