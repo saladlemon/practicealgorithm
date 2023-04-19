d3b1dc291e6302d880b51dc5b0bad4197c0f034d
DoHun Jeon <107619587+saladlemon@users.noreply.github.com>
2023-04-19 18:28:02 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,28 @@
+function solution(array) {
+    const length = array.length;
+    const arr = array.sort();
+    let mode;
+    let Subcount = 0;
+    let count = 0;
+    
+    if(length === 1){
+        mode = arr[0];
+    }else{
+        for(i=0; i<length; i++){
+            if(arr[i] === arr[i+1]){
+                count = count + 1;
+            }else{
+                count = count + 1;
+                if(Subcount < count){
+                    Subcount = count;
+                    mode = arr[i];
+                }else if(Subcount === count){
+                    mode = -1;
+                }
+                count = 0;
+            }
+        }
+    }
+    var answer = mode;
+    return answer;
+}
