8981f7d2eace90e89746dfb332b7555a7b0d1fc0
DoHun Jeon <107619587+saladlemon@users.noreply.github.com>
2023-05-12 15:27:27 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,6 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+for i in range(28):
+    n = int(input())
+    arr = [x for x in arr if x != n]
+print(arr[0])
+print(arr[1])
