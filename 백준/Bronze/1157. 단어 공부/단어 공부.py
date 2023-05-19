f0a072969979c6d40db6d57170fce9950b380705
DoHun Jeon <107619587+saladlemon@users.noreply.github.com>
2023-05-19 16:29:35 +0900

    [Bronze I] Title: 단어 공부, Time: 436 ms, Memory: 156456 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,19 @@
+N = input()
+N_LOW = N.lower()
+Ns = set(N_LOW)
+Nl = list(Ns)
+L = []
+for i in Nl:
+    cnt = 0
+    for j in N_LOW:
+        if i == j:
+            cnt += 1
+    L.append(cnt)
+max_alpha = L.index(max(L))
+Max = max(L)
+del L[max_alpha]
+remove = [x for x in L if x != Max]
+if len(remove) < len(L):
+    print("?")
+else:
+    print(Nl[max_alpha].upper())
